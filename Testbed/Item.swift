9846bf77fb6a5c648e2d90905b6c3b372a5788ef
Alex <alexbguthrie@gmail.com>
2025-05-12 13:58:21 -0400

    Initial Commit

---
+++ Testbed/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Testbed
+//
+//  Created by Alex Guthrie on 5/12/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ TestbedTests/TestbedTests.swift
@@ -0,0 +1,16 @@
+//
+//  TestbedTests.swift
+//  TestbedTests
+//
+//  Created by Alex Guthrie on 5/12/25.
+//
+
+import Testing
+
+struct TestbedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
